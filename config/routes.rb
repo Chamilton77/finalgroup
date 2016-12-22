00f20a6c00a3159269c6e9a75aa2c664273859f6
Carson Hamilton <cjcham77@gmail.com>
2016-12-22 15:44:36 -0700

    This is the create account pages and a little bit of the nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :chefs, controllers: {
-        sessions: 'users/sessions'
+        registrations: 'chefs/registrations'
       }
 
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
+        registrations: 'users/registrations'
       }
 
      resources :requests
+     resources :chefs
+     resources :users
 	 root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
