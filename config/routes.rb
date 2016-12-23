6c56b23fa458e6ea1b135d47cf9a0ba542fe3de7
Carson Hamilton <cjcham77@gmail.com>
2016-12-23 12:24:26 -0700

    testing to see if this works

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-  get 'contact/index'
-
-  get 'chefs/index'
-
-  get 'chefs/show'
-
   devise_for :chefs, controllers: {
         registrations: 'chefs/registrations'
       }
-
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
-
-     resources :requests
+   resources :requests
 	 root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
